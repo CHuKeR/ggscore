346bc9fa153017d0cdb9d61153f6ac2244e97e27
Yuri Chukanov <chuker69@gmail.com>
2017-09-29 13:21:59 +0300

    Сделали включение настроек для юзера, осталось связать с БД

--- bot.py
+++ bot.py
@@ -1,47 +1,59 @@
 import config
 import telebot
-import os
-import time
-from random import shuffle
-from SQLighter import SQLighter
-import random
 import utils
 
+
+
+
 bot = telebot.TeleBot(config.token)
+settings = ["Оповещения о будущих матчах"]
+
+@bot.message_handler(commands=["setting"])
+def get_user_id(message):
+    utils.enable_settings_for_user(message.chat.id)
+    mess = ""
+    num = 1
+    markup = utils.generate_markup(settings)
+    for item in settings:
+        mess+="{}. {}\n".format(num,item)
+    bot.send_message(message.chat.id,
+    """
+    Привет, давай настроим бота.
+    Выбери, что настраивать:\n""" + mess,reply_markup=markup)
+
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def check_answer(message):
+    # Если функция возвращает None -> Человек не в игре
+    answer = utils.get_user_answer(message.chat.id)
+    # Как Вы помните, answer может быть либо текст, либо None
+    # Если None:
+    if not answer:
+        bot.send_message(message.chat.id, 'Если хотите настроек, выберите команду /setting (пока тут больше ничего нет)')
+    else:
+        # Уберем клавиатуру с вариантами ответа.
+        keyboard_hider = telebot.types.ReplyKeyboardRemove()
+        # Если ответ правильный/неправильный
+        if message.text == "1" or settings.index(message.text) == 0:
+            bot.send_message(message.chat.id, 'Верно!', reply_markup=keyboard_hider)
+        else:
+            bot.send_message(message.chat.id, 'Нет такого варианта =О!', reply_markup=keyboard_hider)
+        # Удаляем юзера из хранилища
+        utils.delete_user_settings(message.chat.id)
+
+@bot.message_handler(commands=["322"])
+def get_user_id(message):
+    bot.send_message(message.chat.id,
+"""
+Первое правило ставок - не ставить на Na'Vi.
+Второе правило ставок - не ставь против Na'Vi.
+"""
+)
 
 @bot.message_handler(commands=["get_id"])
 def get_user_id(message):
     bot.send_message(message.chat.id,message.chat.id)
-
-
-@bot.message_handler(commands=["test"])
-def find_file_ids(message):
-    for file in os.listdir('music/'):
-        if file.split(".")[-1] == "ogg":
-            f = open("music/"+file,"rb")
-            msg = bot.send_voice(message.chat.id,f,None,10)
-            bot.send_message(message.chat.id,msg.voice.file_id,reply_to_message_id=msg.message_id)
-        time.sleep(3)
-
-@bot.message_handler(commands=['game'])
-def game(message):
-    # Подключаемся к БД
-    db_worker = SQLighter(config.database_name)
-    # Получаем случайную строку из БД
-    row = db_worker.select_single(random.randint(1, utils.get_rows_count()))
-    # Формируем разметку
-    markup = utils.generate_markup(row[2], row[3])
-    # Отправляем аудиофайл с вариантами ответа
-    bot.send_voice(message.chat.id, row[1], reply_markup=markup)
-    # Включаем "игровой режим"
-    utils.set_user_game(message.chat.id, row[2])
-    # Отсоединяемся от БД
-    db_worker.close()
-
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
-
 
 if __name__=="__main__":
     bot.polling(none_stop=True)
--- utils.py
+++ utils.py
@@ -1,30 +1,12 @@
 import shelve
-from SQLighter import SQLighter
-from config import shelve_name, database_name
+from config import shelve_name
+from telebot.types import ReplyKeyboardMarkup
 
-
-def count_rows():
-    db = SQLighter(database_name)
-    rowsnum = db.count_rows()
+def enable_settings_for_user(chat_id):
     with shelve.open(shelve_name) as storage:
-        storage["rows_count"]= rowsnum
+        storage[str(chat_id)] = True
 
-def get_row_count():
-    with shelve.open(shelve_name) as storage:
-        rowsnum = storage["rows_count"]
-    return rowsnum
-
-def set_user_game(chat_id, estimated_answer):
-    """
-    Записываем юзера в игроки и запоминаем, что он должен ответить.
-    :param chat_id: id юзера
-    :param estimated_answer: правильный ответ (из БД)
-    """
-    with shelve.open(shelve_name) as storage:
-        storage[str(chat_id)] = estimated_answer
-
-
-def finish_user_game(chat_id):
+def delete_user_settings(chat_id):
     """
     Заканчиваем игру текущего пользователя и удаляем правильный ответ из хранилища
     :param chat_id: id юзера
@@ -33,17 +15,34 @@ def finish_user_game(chat_id):
         del storage[str(chat_id)]
 
 
-def get_answer_for_user(chat_id):
+def generate_markup(setting):
     """
-    Получаем правильный ответ для текущего юзера.
-    В случае, если человек просто ввёл какие-то символы, не начав игру, возвращаем None
-    :param chat_id: id юзера
-    :return: (str) Правильный ответ / None
+        Создаем кастомную клавиатуру для выбора ответа
+        :param right_answer: Правильный ответ
+        :param wrong_answers: Набор неправильных ответов
+        :return: Объект кастомной клавиатуры
+        """
+    list_items = []
+    markup = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    # Склеиваем правильный ответ с неправильными
+    for item in setting:
+        list_items.append(item)
+    # Заполняем разметку перемешанными элементами
+    for item in list_items:
+        markup.add(item)
+    return markup
+
+def get_user_answer(chat_id):
     """
+        Получаем правильный ответ для текущего юзера.
+        В случае, если человек просто ввёл какие-то символы, не начав игру, возвращаем None
+        :param chat_id: id юзера
+        :return: (str) Правильный ответ / None
+        """
     with shelve.open(shelve_name) as storage:
         try:
             answer = storage[str(chat_id)]
             return answer
         # Если человек не играет, ничего не возвращаем
         except KeyError:
-            return Nonex
+            return None
