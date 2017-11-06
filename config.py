0c12fd676c57ed05a36f441b5b08b44ec736adb1
CHuKeR <chuker69@gmail.com>
2017-11-06 23:12:33 +0300

    fix_token

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-token = "421285495:AAGd1N4zW2UuWWajsswD-QYkLaKdLGHp8pk"
+token = "451062564:AAHefQrRh8J--B8HdCblBkJpo3aRV6Ci4Eg"
 setting_enable = 'shelve_set_enable.db' # Файл с хранилищем настроек (включен\выключен)
 setting_mode = 'shelve_set_mod.db' # Файл с хранилищем настроек (какой режим выбран)
