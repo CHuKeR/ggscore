79262575e2c3b88ed63f6963f8fdb00cebdc104b
CHuKeR <chuker69@gmail.com>
2017-11-21 20:09:27 +0300

    fix bot

--- SQLighter.py
+++ SQLighter.py
@@ -4,6 +4,7 @@ from sys import platform
 class DotaSqlClient:
 
     def __init__(self):
+        print(platform)
         if platform == "win32":
             self.connection = pymysql.connect(host='localhost',
                                               port=3306,
@@ -11,9 +12,7 @@ class DotaSqlClient:
                                               passwd='123',
                                               db='heroku_16092c835aedf9e',
                                               charset="utf8")
-
         else:
-
             self.connection = pymysql.connect(host='us-cdbr-iron-east-05.cleardb.net',
                                           port=3306,
                                           user='b1c0c21dcb6edc',
