719352e707748eacef6ab78195d829994a2b3dbe
CHuKeR <chuker69@gmail.com>
2017-11-23 00:08:06 +0300

    fix update_db

--- Update_db.py
+++ Update_db.py
@@ -7,6 +7,5 @@ if __name__ == "__main__":
     sqler = SQLighter.DotaSqlClient()
     dp = dota_parser_lib.dota_parser(sqler)
     dp.update_matches()
-    sqler.close()
     print('Update DB from cron')
 
