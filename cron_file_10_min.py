ada64e3dca753b40542b1a28313fba9911018aad
CHuKeR <chuker69@gmail.com>
2020-07-03 21:14:36 +0300

    add update finished matches

--- cron_file_10_min.py
+++ cron_file_10_min.py
@@ -5,4 +5,5 @@ if __name__ == '__main__':
     update_matches = update_future_dota_matches()
     if len(update_matches) > 0:
         send_updated_matches_to_user(update_matches)
+    update_finished_dota_matches()
     send_finished_matches_to_users()
